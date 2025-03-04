26a8cecd2dc754a06d3ecb0e7dfbe40a9d6e7f70
hewenbin <405159130@qq.com>
2025-03-05 07:35:18 +0800

    记录用时方法：实现带参数的装饰器工厂，支持传参；

--- src/_decorator/logExecutionTime.ts
+++ src/_decorator/logExecutionTime.ts
@@ -1,27 +1,29 @@
-export function logExecutionTime(
-    target: any, // 目标类或原型
-    propertyKey: string, // 方法名
-    descriptor: PropertyDescriptor // 属性描述符
-) {
-    const originalMethod = descriptor.value; // 原始方法
-    if (typeof originalMethod === 'function') {
-        descriptor.value = function (...args: any[]) {
-            const start = performance.now();
-            const result = originalMethod.apply(this, args); // 调用原始方法
-            if (result instanceof Promise) {
-                // 如果是异步函数，等待 Promise 完成
-                return result.then((res) => {
+export function logExecutionTime(title: string) {
+    return function (
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value; // 原始方法
+        if (typeof originalMethod === 'function') {
+            descriptor.value = function (...args: any[]) {
+                const start = performance.now();
+                const result = originalMethod.apply(this, args); // 调用原始方法
+                if (result instanceof Promise) {
+                    // 如果是异步函数，等待 Promise 完成
+                    return result.then((res) => {
+                        const end = performance.now();
+                        console.log(`${title} - ${propertyKey} executed in ${(end - start).toFixed(2)}ms`);
+                        return res;
+                    });
+                } else {
+                    // 如果是同步函数，直接记录时间
                     const end = performance.now();
-                    console.log(`${propertyKey} executed in ${(end - start).toFixed(2)}ms`);
-                    return res;
-                });
-            } else {
-                // 如果是同步函数，直接记录时间
-                const end = performance.now();
-                console.log(`${propertyKey} executed in ${(end - start).toFixed(2)}ms`);
-                return result;
-            }
-        };
-    }
-    return descriptor; // 返回修改后的属性描述符
+                    console.log(`${title} - ${propertyKey} executed in ${(end - start).toFixed(2)}ms`);
+                    return result;
+                }
+            };
+        }
+        return descriptor; // 返回修改后的属性描述符
+    };
 }
